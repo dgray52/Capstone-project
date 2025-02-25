7db1e19e080778dc6352c2fafc19cfd49e8244f8
DG-DEV <dg-dev@pop-os.dyn.uncc.edu>
2025-02-25 15:37:13 -0500

    mostly finished app.js

--- capstone/app.js
+++ capstone/app.js
@@ -1,3 +1,25 @@
 const express=require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const { DateTime } = require("luxon");
+const methodOverride=require('method-override');
+const morgan=require('morgan');
+const SwapRoutes=require('./routers/swaprouter');
+const MatchRoutes=require('./routers/matchrouter');
+
+const app=express();
+
+let port=3000;
+let host='localhost';
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+app.get('/',(req,res) =>{
+    res.render('index');    
+});
+app.use('/matches',MatchRoutes);
+app.use('/swaps',SwapRoutes);
+app.listen(port,host, () =>{
+    console.log('test');});
