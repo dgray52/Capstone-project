1900ca9a9256973836cb16e71061c97e2ddb810e
Daniel <dgray52@uncc.edu>
2025-02-27 10:22:51 -0500

    fixed swaprouter syntax error

--- capstone/routers/swaprouter.js
+++ capstone/routers/swaprouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/matchcontroller');
+const controller = require('../controllers/swapcontroller');
 // / index
 router.get('/', controller.index);
 //get /swaps/new 
